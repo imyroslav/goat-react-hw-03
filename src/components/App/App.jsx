9cadbb6154b9fe8ad9130c98860b7739696e55b2
imyroslav <imyroslav@ukr.net>
2024-05-18 20:28:29 -0600

    created-appjsx

---
+++ src/components/App/App.jsx
@@ -0,0 +1,18 @@
+import Contact from "../Contact/Contact";
+import ContactForm from "../ContactForm/ContactForm";
+import ContactList from "../ContactList/ContactList";
+import SearchBox from "../SearchBox/SearchBox";
+import css from "./App.module.css";
+
+export default function App() {
+    
+    return (
+        <div>
+            <h1>Phonebook</h1>
+            <ContactForm />
+            <SearchBox />
+            <ContactList />
+        </div>
+
+    )
+}
