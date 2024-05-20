d1668c739f992ab98865524bcf6e191d11a43803
imyroslav <imyroslav@ukr.net>
2024-05-19 20:01:42 -0600

    add-component-contact-form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,18 +1,62 @@
-export default function ContactForm() {
+import { useId } from 'react';
+import { Formik, Form, Field } from 'formik';
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
+import css from "./ContactForm.module.css";
 
+export default function ContactForm({ addContact, validationSchema }) {
+  const initialValues = {
+    name: '',
+    phoneNumber: '',
+  };
+
+  const [submitAttempted, setSubmitAttempted] = useState(false);
+
+  const nameFieldId = useId();
+  const phoneNumberFieldId = useId();
+
+  const handleSubmit = (values, actions) => {
+    const id = nanoid();
+    addContact({ id, name: values.name, number: values.phoneNumber });
+    actions.resetForm();
+  };
+
+    
     return (
-        <form>
-            <div>
-                <label>Name</label>
-                <input type="text" name="name" />
-            </div>
-
-            <div>
-                <label>Number</label>
-                <input type="text" name="number" />
-            </div>
-
-            <button>Add contact</button>
-        </form>
+        <Formik
+            initialValues={initialValues}
+            onSubmit={handleSubmit}
+            validationSchema={validationSchema}
+        >
+            {({ errors, touched }) => (
+          <Form className={css.form}>
+            <label htmlFor={nameFieldId}>Name</label>
+            <Field
+              type="text"
+              name="name"
+              className={css.field}
+              id={nameFieldId}
+              onBlur={() => setSubmitAttempted(true)}
+            />
+            {(submitAttempted || touched.name) && errors.name && (
+              <div className="error">{errors.name}</div>
+            )}
+            <label htmlFor={phoneNumberFieldId}>Number</label>
+            <Field
+              type="text"
+              name="phoneNumber"
+              className={css.field}
+              id={phoneNumberFieldId}
+              onBlur={() => setSubmitAttempted(true)}
+            />
+            {(submitAttempted || touched.phoneNumber) && errors.phoneNumber && (
+              <div className="error">{errors.phoneNumber}</div>
+            )}
+            <button type="submit" className={css.btn}>
+              Add contact
+            </button>
+          </Form>
+        )}
+        </Formik>
     )
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,3 +1,5 @@
+// import css from "./ContactList.module.css";
+
 export default function ContactList() {
 
     return 
